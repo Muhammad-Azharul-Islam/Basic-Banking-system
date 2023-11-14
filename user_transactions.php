996f9e4cdc90a090d7dc8676bc4b626c482caa8c
Muhammad Azharul Islam <127438039+Azhar699@users.noreply.github.com>
2023-11-14 21:12:42 +0530

    Update user_transactions.php

--- user_transactions.php
+++ user_transactions.php
@@ -42,7 +42,7 @@ if (isset($_GET['id'])) {
 
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="./history.css">
+    <link rel="stylesheet" href="css/history.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>
         <?php echo $fName . " " . $lName . " History" ?>
