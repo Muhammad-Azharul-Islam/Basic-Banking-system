57e7fd3cad367952a014f94b8661fadc3b15acdb
Muhammad Azharul Islam <127438039+Azhar699@users.noreply.github.com>
2023-11-14 21:13:00 +0530

    Update transfer.php

--- transfer.php
+++ transfer.php
@@ -82,7 +82,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['send'])) {
 
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="./transfer.css">
+    <link rel="stylesheet" href="css/transfer.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Basic Banking System - Transfer </title>
 </head>
